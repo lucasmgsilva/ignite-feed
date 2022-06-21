4131614c4a0b40bf1c254cadcd9bb62290fe66bf
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-06-21 19:46:11 -0300

    CSS modules, CSS global e componente header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Header } from './components/Header';
+
+import './global.css';
+
+function App() {
+  return (
+    <Header />
+  )
+}
+
+export default App

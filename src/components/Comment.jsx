344b224d397a559e806ec98654f03ab73121ce0d
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-07-06 13:13:29 -0300

    Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+    
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://scontent.fsjp8-1.fna.fbcdn.net/v/t39.30808-6/241318847_3629242633845418_8967739168686586527_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=pNrVJIlxEz0AX9aK_1m&_nc_ht=scontent.fsjp8-1.fna&oh=00_AT_yi6ghsRBhHDkqd2vfzBF4rsWLXPolgUKg5tFB0nuR1g&oe=62CA0C81" />
@@ -17,7 +22,7 @@ export function Comment({ content }) {
                             <time dateTime="2022-05-11 08:13:30" title="11 de Maio às 08:13h">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24}/>
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -37,6 +37,10 @@ export function Post({ author, content, publishedAt }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(comment) {
+        console.log(`Deletar comentário ${comment}`);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -77,7 +81,13 @@ export function Post({ author, content, publishedAt }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>
+                    return (
+                        <Comment 
+                            key={comment}
+                            content={comment}
+                            onDeleteComment={deleteComment}
+                        />
+                    )
                 })}
             </div>
         </article>

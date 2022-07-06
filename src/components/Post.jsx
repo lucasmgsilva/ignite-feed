46657665f9349b189a609e483edfe1eb04d58c13
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-07-06 14:10:08 -0300

    Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,7 +34,12 @@ export function Post({ author, content, publishedAt }) {
     }
 
     function handleNewCommentChange(event){
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(event){
+        event.target.setCustomValidity('Por favor, digite um comentário!');
     }
 
     function deleteComment(commentToDelete) {
@@ -44,6 +49,8 @@ export function Post({ author, content, publishedAt }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -76,10 +83,14 @@ export function Post({ author, content, publishedAt }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
